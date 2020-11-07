43ea3c16ec709b70acaf765b8044bf8b9950454d
wanglin-it <wanglin_it@163.com>
2020-11-07 09:33:54 +0800

    test01文件创建

---
+++ src/com/mashibing/Thread/test01.java
@@ -0,0 +1,13 @@
+package com.mashibing.Thread;
+
+/**
+ * @author : WLIN
+ * @description :
+ * @date :2020/11/7
+ */
+public class test01 {
+
+    public static void main(String[] args) {
+
+    }
+}
